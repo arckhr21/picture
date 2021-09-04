f235886210b2e2d064785f47b5ab9743009ddd71
arckhr21 <arcad.khrulev@gmail.com>
2021-09-04 19:02:39 +0300

    checkTextInputs

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -12,4 +13,6 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.main-slider-item', 'vertical');
     forms();
     mask('[name = "phone"]');
+    checkTextInputs('[name = "name"]');
+    checkTextInputs('[name = "message"]');
 });
