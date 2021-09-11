0f40d86caa060527881a32f46aea6c3b75e9650c
arckhr21 <arcad.khrulev@gmail.com>
2021-09-11 09:02:59 +0300

    calc

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
-
+import calc from './modules/calc';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -13,8 +13,9 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
-    mask('[name = "phone"]');
-    checkTextInputs('[name = "name"]');
-    checkTextInputs('[name = "message"]');
-    showMoreStyles('.button-styles', '.styles-2');
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '#styles .row');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price');
 });
